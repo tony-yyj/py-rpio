a8128420993cc3f684cf96a6fef3fef23eb2de79
tony <tonycs_y@126.com>
2018-02-07 00:21:13 +0800

    fix

--- charlieplexing.py
+++ charlieplexing.py
@@ -1,5 +1,5 @@
 import RPi.GPIO as GPIO
-
+import time;
 pins = [18, 23, 24]
 pin_led_status = [
     [1, 0, -1],  # A
@@ -30,6 +30,10 @@ set_pin(0, -1)
 set_pin(1, -1)
 set_pin(2, -1)
 
+
+round_times = [0, 0, 3, 2, 5, 1, 3, 2, 3, 3, 3, 1, 3, 2, 3, 5, 2, 2];
+
 while True:
-    x = int(raw_input("Pin (0 to 5):"))
-    light_led(x)
+    for t in round_times:
+        light_led(t)
+        time.sleep(0.5)
