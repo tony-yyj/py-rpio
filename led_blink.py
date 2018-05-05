a434161b7502232bca9dd91bd536802265790ca6
tony <tonycs_y@126.com>
2018-05-05 17:52:39 +0800

    点亮led

--- led_blink.py
+++ led_blink.py
@@ -1,13 +1,19 @@
+# coding: utf-8
 import RPi.GPIO as GPIO
 
 import time
-
+# 选择引脚的模式为板载模式
 GPIO.setmode(GPIO.BCM)
-
+# 将18号引脚设置为输出模式
 GPIO.setup(18, GPIO.OUT)
 
 while True:
+    # 将18号输出引脚设置为高电平
     GPIO.output(18, True)
     time.sleep(0.5)
+    # 低电平
     GPIO.output(18, False)
+    # 休眠500毫秒
     time.sleep(0.5)
+
+
